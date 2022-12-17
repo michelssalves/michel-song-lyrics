91a870214f453b44a0cc9e5481d2b2395fd9ee42
unknown <michelssalves@gmail.com>
2022-12-17 00:26:30 -0300

    alterado titulo Ref: 17-12-2022 - 00:26 -> MICHEL

--- view/menuLateral.view.php
+++ view/menuLateral.view.php
@@ -1,7 +1,7 @@
 <body class="app sidebar-mini">
     
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="?p=1"><i class="fa-solid fa-gas-pump"></i>Controle Postos</a>
+    <header class="app-header"><a class="app-header__logo" href="?p=1"><i class="fa-solid fa-gas-pump"></i>Louvores</a>
         <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
         <ul class="app-nav">
             <!-- Navbar Right Menu
@@ -40,13 +40,13 @@
         <ul class="app-menu">
             <li><a class="app-menu__item <?= $active[2]; ?>" href="?p=2"><i class="app-menu__icon fa-solid fa-money-check-dollar"></i><span class="app-menu__label">Cheques</span></a></li>
             <li><a class="app-menu__item <?= $active[3]; ?>" href="?p=3"><i class="app-menu__icon fa-solid fa-sack-dollar"></i><span class="app-menu__label">Depositos</span></a></li>
-            <li><a class="app-menu__item <?= $active[4]; ?>" href="?p=4"><i class="app-menu__icon fa-solid fa-coins"></i><span class="app-menu__label">Cx Diário</span></a></li>
+            <li><a class="app-menu__item <?= $active[4]; ?>" href="?p=4"><i class="app-menu__icon fa-solid fa-coins"></i><span class="app-menu__label">Cx Diï¿½rio</span></a></li>
             <li><a class="app-menu__item <?= $active[5]; ?>" href="?p=5"><i class="app-menu__icon fa-solid fa-ban"></i><span class="app-menu__label">Serasa</span></a></li>
             <li><a class="app-menu__item <?= $active[6]; ?>" href="?p=6"><i class="app-menu__icon fa-solid fa-truck-fast"></i><span class="app-menu__label">Envios de Material</span></a></li>
             <li><a class="app-menu__item <?= $active[7]; ?>" href="?p=7"><i class="app-menu__icon fa-solid fa-comments-dollar"></i><span class="app-menu__label">Sol Pagamentos</span></a></li>
             <li><a class="app-menu__item <?= $active[8]; ?>" href="?p=8"><i class="app-menu__icon fa-solid fa-file-pdf"></i><span class="app-menu__label">Documentos</span></a></li>
             <li><a class="app-menu__item <?= $active[9]; ?>" href="?p=9"><i class="app-menu__icon fa-solid fa-chart-line"></i><span class="app-menu__label">Indic Operacionais</span></a></li>
-            <li><a class="app-menu__item <?= $active[10]; ?>" href="?p=10"><i class="app-menu__icon fa-solid fa-boxes-stacked"></i><span class="app-menu__label">Inventário</span></a></li>
+            <li><a class="app-menu__item <?= $active[10]; ?>" href="?p=10"><i class="app-menu__icon fa-solid fa-boxes-stacked"></i><span class="app-menu__label">Inventï¿½rio</span></a></li>
             <li><a class="app-menu__item <?= $active[11]; ?>" href="?p=11"><i class="app-menu__icon fa-solid fa-calculator"></i><span class="app-menu__label">Calculo Manual</span></a></li>
             <li><a class="app-menu__item <?= $active[12]; ?>" href="?p=12"><i class="app-menu__icon fa-solid fa-flask-vial"></i><span class="app-menu__label">Anl Combustivel</span></a></li>
             <li><a class="app-menu__item <?= $active[13]; ?>" href="?p=13"><i class="app-menu__icon fa-solid fa-magnifying-glass-chart"></i><span class="app-menu__label">Ctrl Qualidade</span></a></li>
